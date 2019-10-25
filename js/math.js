9ec0ced7a2a0a756c0ea7a5bdeaf3a6ebbebbd95
Garrett Trott <garretttrott@gmail.com>
2019-10-24 23:26:35 -0700

    add fuctions and refactoring

--- js/math.js
+++ js/math.js
@@ -1,17 +1,37 @@
-alert("Let's do math!");
 
-let number1 = parseFloat(prompt("Enter a number: "));
-let number2 = parseFloat(prompt("Enter a second number: "));
+let number1;
+let number2;
 
-let add = number2 + number2;
-let subtract = number1 - number2;
-let divide = number1 / number2.toFixed(5);
-let multiply = number1 * number2;
+function doMath(numb1, numb2){
 
-let content = `<h1>Math with numbers ${number1} & ${number2}</h1>
+  let add;
+  let subtract;
+  let divide;
+  let multiply;
+
+  add = numb1 + numb2;
+  subtract = numb1 - numb2;
+  divide = numb1 / numb2.toFixed(5);
+  multiply = numb1 * numb2;
+
+  content = `<h1>Math with numbers ${number1} & ${number2}</h1>
                <p>${number1} + ${number2} = ${add}</p>
                <p>${number1} - ${number2} = ${subtract}</p>
                <p>${number1} / ${number2} = ${divide}</p>
                <p>${number1} * ${number2} = ${multiply}</p>`;
 
-document.getElementById("content").innerHTML = content;
+  return document.getElementById("content").innerHTML = content;
+}; 
+
+alert("Let's do math!");
+
+number1 = parseFloat(prompt("Enter a number: "));
+
+
+if isNaN (number1) {
+  alert("That is not number!")
+}
+
+number2 = parseFloat(prompt("Enter a second number: "));
+
+
