933f8577445cfc3e6b724163f48c083c65deaee8
Garrett Trott <garretttrott@gmail.com>
2019-10-28 13:05:50 -0700

    added conditonal statements

--- js/math.js
+++ js/math.js
@@ -1,9 +1,7 @@
-
 let number1;
 let number2;
 
-function doMath(numb1, numb2){
-
+function doMath(numb1, numb2) {
   let add;
   let subtract;
   let divide;
@@ -20,18 +18,18 @@ function doMath(numb1, numb2){
                <p>${number1} / ${number2} = ${divide}</p>
                <p>${number1} * ${number2} = ${multiply}</p>`;
 
-  return document.getElementById("content").innerHTML = content;
-}; 
+  return (document.getElementById("content").innerHTML = content);
+}
 
 alert("Let's do math!");
 
 number1 = parseFloat(prompt("Enter a number: "));
-
-
-if isNaN (number1) {
-  alert("That is not number!")
-}
-
 number2 = parseFloat(prompt("Enter a second number: "));
 
-
+if (isNaN(number1) || isNaN(number2)) {
+  alert("One of the the two inputs is not a number, reload to start again.");
+} else if (number2 === 0) {
+  alert("0 is not divisable please reload and enter a number other than 0.");
+} else {
+  doMath(number1, number2);
+}
